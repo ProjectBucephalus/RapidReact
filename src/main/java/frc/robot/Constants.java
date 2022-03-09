5ab50d53460db3b1a75da7e62a62de82c0edb5b6
LordBBQ <46541720+LordBBQ@users.noreply.github.com>
2022-03-09 18:37:07 +1100

    Tuning for climber

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -49,8 +49,9 @@ public class Constants {
 	public static final double kShooterP = 0.25;
 	public static final double kShooterI = 0.00014;
 
-	public static final double kClimberWinchP = 0.005;
+	public static final double kClimberWinchP = 0.029;
 
+	
 	public static final double kIdleCurrent = 1; //Ampres
 	public static final double kIdleVoltageCutoff = 12.2; //volts
 
