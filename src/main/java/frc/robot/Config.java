d89785269c526c2da740fa9a1517d635617ce9ab
LordBBQ <46541720+LordBBQ@users.noreply.github.com>
2022-03-10 21:21:41 +1100

    Updated version number

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -5,10 +5,10 @@ import frc.robot.DriverInterface.VersionType;
 public class Config {
 
     //Version number. Update every pull request, release, or major change!
-    public static final String version = "1.0.0 - RC1";
+    public static final String version = "1.0.0";
 
     //Version type
-    public static final VersionType versionType = VersionType.RELEASECANDIDATE;
+    public static final VersionType versionType = VersionType.RELEASE
     
     //1 = normal, -1 = inverted
     public static final double kInvertDir = 1;
